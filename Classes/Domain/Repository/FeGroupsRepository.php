367a3144fe4c82b568b544decb226a0e9ab60881
Sebastian Stein <sebastian.stein@in2code.de>
2020-01-21 16:44:43 +0100

    [FEATURE] add ip v6 compatibility

--- Classes/Domain/Repository/FeGroupsRepository.php
+++ Classes/Domain/Repository/FeGroupsRepository.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace In2code\In2frontendauthentication\Domain\Repository;
 
 /***************************************************************
@@ -25,8 +26,10 @@ namespace In2code\In2frontendauthentication\Domain\Repository;
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
+use IPLib\Factory;
 use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Database\Query\Expression\CompositeExpression;
+use TYPO3\CMS\Core\Database\Query\QueryBuilder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
@@ -37,30 +40,36 @@ class FeGroupsRepository
     const TABLE_NAME = 'fe_groups';
 
     /**
+     * @var QueryBuilder
+     */
+    protected $queryBuilder;
+
+    /**
+     * FeGroupsRepository constructor.
+     */
+    public function __construct()
+    {
+        $this->queryBuilder =
+            GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable(self::TABLE_NAME);
+    }
+
+    /**
      * Find groups with current IP address
      *
      * @return array
      */
     public function findByCurrentIpAddress()
     {
-        /**
-         * @var $queryBuilder \TYPO3\CMS\Core\Database\Query\QueryBuilder
-         */
-        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)->getQueryBuilderForTable(self::TABLE_NAME);
-
-        /**
-         * @var $statement \Doctrine\DBAL\Driver\Mysqli\MysqliStatement
-         */
-        $statement = $queryBuilder->select('*')
+        $statement = $this->queryBuilder->select('*')
             ->from(self::TABLE_NAME)
             ->where(
-                $queryBuilder->expr()->eq('deleted', 0),
-                $queryBuilder->expr()->eq('hidden', 0),
-                $this->getIpQueryExpression($queryBuilder)
+                $this->queryBuilder->expr()->eq('deleted', 0),
+                $this->queryBuilder->expr()->eq('hidden', 0),
+                $this->getIpQueryExpression()
             )
             ->execute();
 
-        if($statement->rowCount()) {
+        if ($statement->rowCount()) {
             return $statement->fetchAll();
         }
 
@@ -73,13 +82,14 @@ class FeGroupsRepository
      *
      * @return CompositeExpression
      */
-    protected function getIpQueryExpression($queryBuilder)
+    protected function getIpQueryExpression()
     {
-        $currentIpAdresses = $this->getCurrentIpAddresses();
-        if (!empty($currentIpAdresses)) {
-            $orX = $queryBuilder->expr()->orX();
-            foreach ($currentIpAdresses as $value) {
-                $orX->add($queryBuilder->expr()->like('ip_mask', "'%$value%'"));
+        $currentIpAddresses = $this->getCurrentIpAddresses();
+        $orX = $this->queryBuilder->expr()->orX();
+
+        if (!empty($currentIpAddresses)) {
+            foreach ($currentIpAddresses as $value) {
+                $orX->add($this->queryBuilder->expr()->like('ip_mask', "'%$value%'"));
             }
         }
 
@@ -93,23 +103,80 @@ class FeGroupsRepository
      */
     protected function getCurrentIpAddresses()
     {
-        $ips = [
-            GeneralUtility::getIndpEnv('REMOTE_ADDR')
-        ];
-        $parts = GeneralUtility::trimExplode('.', $ips[0], true);
-        $ips[] = '*.*.*.*';
-        $ips[] = $parts[0] . '.*.*.*';
-        $ips[] = $parts[0] . '.' . $parts[1] . '.*.*';
-        $ips[] = $parts[0] . '.' . $parts[1] . '.' . $parts[2] . '.*';
-        return $ips;
+        $ip = GeneralUtility::getIndpEnv('REMOTE_ADDR');
+        $address = Factory::addressFromString($ip);
+
+        if ($address->getAddressType() === 4) {
+            // ip v4
+            $delimiter = '.';
+            $useFullIp = false;
+        } else {
+            // ip v6
+            $delimiter = ':';
+            $useFullIp = true;
+        }
+
+        $wildcards = $this->buildWildcardArray($address->toString($useFullIp), $delimiter);
+
+        // the actual current ip
+        $ips = [$ip];
+
+        // it is possible that the user ip address is shortened
+        // (e.g. 2001::08d3:1319:8a2e:0370:1334 instead of 2001:0000:0000:08d3:1319:8a2e:0370:1334) so we add also the
+        // full ip to the check.
+        if ($address->toString(true) !== $ip) {
+            $ips[] = $address->toString(true);
+        }
+
+        return array_merge($wildcards, $ips);
     }
 
     /**
-     * @return DatabaseConnection
-     * @SuppressWarnings(PHPMD.Superglobals)
+     * builds an wildcard array by an given ip e.g.
+     *
+     *      if the following ip is given: 127.0.0.1
+     *
+     *      returns:
+     *      [
+     *            *.*.*.*
+     *          127.*.*.*
+     *          127.0.*.*
+     *          127.0.0.*
+     *      ]
+     *
+     *      if an ip v6 ip is given: 2001:0000:0000:08d3:1319:8a2e:0370:1334
+     *
+     *      returns:
+     *      [
+     *          *:*:*:*:*:*:*:*
+     *          2001:*:*:*:*:*:*:*
+     *          2001.0000*:*:*:*:*:*
+     *          ...
+     *      ]
+     *
+     * @param string $ip
+     * @param string $delimiter
+     * @param string $wildcardChar
+     * @return array
      */
-    protected function getDatabaseConnection()
+    protected function buildWildcardArray($ip, $delimiter, $wildcardChar = '*')
     {
-        return $GLOBALS['TYPO3_DB'];
+        $parts = GeneralUtility::trimExplode($delimiter, $ip, true);
+        $wildcardArray = [];
+
+        for ($key = 0; $key < count($parts); $key++) {
+            $wildcardString = '';
+            for ($i = 0; $i < count($parts); $i++) {
+                if ($i >= $key) {
+                    $wildcardString .= $wildcardChar . $delimiter;
+                } else {
+                    $wildcardString .= $parts[$i] . $delimiter;
+                }
+            }
+
+            $wildcardArray[] = rtrim($wildcardString, $delimiter);
+        }
+
+        return $wildcardArray;
     }
 }
