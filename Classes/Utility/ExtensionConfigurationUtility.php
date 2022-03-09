785170e6c7954aa10014a7eee2823a3984a93227
Marcus Schwemer <marcus.schwemer@in2code.de>
2022-03-09 09:15:42 +0100

    [CLEANUP] Remove dead code for TYPO3 versions V8 and older
    The removed code was not executed anymore, because the version requirements
    were already set to V9+.
    
    Signed-off-by: Marcus Schwemer <marcus.schwemer@in2code.de>

--- Classes/Utility/ExtensionConfigurationUtility.php
+++ Classes/Utility/ExtensionConfigurationUtility.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 namespace In2code\In2frontendauthentication\Utility;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Utility\VersionNumberUtility;
-use TYPO3\CMS\Extbase\Object\ObjectManager;
 
 class ExtensionConfigurationUtility
 {
@@ -15,24 +13,8 @@ class ExtensionConfigurationUtility
      */
     public static function getExtensionConfiguration($path, $extensionKey = 'in2frontendauthentication'): string
     {
-        $version = \TYPO3\CMS\Core\Utility\VersionNumberUtility::convertVersionStringToArray(
-            VersionNumberUtility::getCurrentTypo3Version()
-        );
-
-        // TYPO3 9 and above
-        if ($version['version_main'] >= 9) {
-            $value = (string)GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)
-                ->get($extensionKey, $path);
-        } else {
-            // TYPO3 8 and below
-            $extConfiguration = GeneralUtility::makeInstance(ObjectManager::class)
-                ->get(\TYPO3\CMS\Extensionmanager\Utility\ConfigurationUtility::class)
-                ->getCurrentConfiguration($extensionKey);
-
-            $value = (string)$extConfiguration[$path]['value'];
-        }
-
-        return $value;
+        return (string)GeneralUtility::makeInstance(\TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class)
+            ->get($extensionKey, $path);
     }
 
     /**
