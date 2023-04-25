bd2f84bae3a3ce7fcc53262d449b80b56367f8d9
Alexander Kellner <alexander.kellner@in2code.de>
2023-04-25 11:38:48 +0200

    [BUGFIX] Adjust requirements in ext_emconf.php to composer.json to support also TYPO3 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'in2code.de',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-11.99.99',
+            'typo3' => '10.4.0-11.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
